4066fe0d705f7c74cf24d86f8e8d4c65a407128d
Gastón Elordi <gaston.elordi@gmail.com>
2017-11-17 20:42:47 -0300

    Seeders fix

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -2,17 +2,15 @@
 
 use Faker\Generator as Faker;
 
-$categories = \App\Category::all();
-$brands = \App\Brand::all();
-
-
-$factory->define(App\Product::class, function (Faker $faker) use ($categories, $brands) {
+$factory->define(App\Product::class, function (Faker $faker) {
+	$category = \App\Category::inRandomOrder()->first();
+	$brand = \App\Brand::inRandomOrder()->first();
     return [
         "name" => $faker->word,
         "stock" => $faker->numberBetween(0,100),
         "price" => $faker->randomFloat(NULL,0, 10000),
         "description" => $faker->text,
-        "category_id" => $categories->random()->id ,
-        "brand_id" => $brands->random()->id
+        "category_id" => $category->id ,
+        "brand_id" => $brand->id
     ];
 });
